dbe7dc60f668515bbd4777dd999ce31525f8741f
vcarvalh <vh.crvlh@gmail.com>
2022-05-16 14:28:49 -0300

    Fixing ft_memmove.c

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,11 +6,13 @@
 /*   By: vcarvalh <vcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 11:39:23 by vcarvalh          #+#    #+#             */
-/*   Updated: 2022/05/12 14:14:08 by vcarvalh         ###   ########.fr       */
+/*   Updated: 2022/05/16 14:27:41 by vcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
@@ -19,11 +21,12 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	if (dest == src || n == 0)
 		return (dest);
 	i = 0;
-	if (src > dest)
+	if (src < dest)
 	{
 		while (i < n)
 		{
 			((char *)dest)[n - 1] = ((char *)src)[n - 1];
+			n--;
 		}
 	}
 	else
