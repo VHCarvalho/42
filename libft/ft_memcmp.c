50026256547623c55a21c0638a55bbc5848570dd
vcarvalh <vh.crvlh@gmail.com>
2022-05-16 13:44:30 -0300

    Fixing ft_memcmp

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,11 +6,13 @@
 /*   By: vcarvalh <vcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 10:28:46 by vcarvalh          #+#    #+#             */
-/*   Updated: 2022/05/12 10:47:27 by vcarvalh         ###   ########.fr       */
+/*   Updated: 2022/05/16 13:37:29 by vcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
@@ -19,11 +21,11 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned char	*ptr2;
 	size_t			i;
 
-	ptr1 = (unsigned char *)&s1;
-	ptr2 = (unsigned char *)&s2;
+	ptr1 = (unsigned char *)s1;
+	ptr2 = (unsigned char *)s2;
 	ret = 0;
 	i = 0;
-	while (i < n)
+	while (n--)
 	{
 		if (ptr1[i] != ptr2[i])
 		{
