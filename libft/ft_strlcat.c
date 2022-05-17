5587e32fcb3543ff3e32227e062894104195e106
vcarvalh <vh.crvlh@gmail.com>
2022-05-17 08:32:49 -0300

    Fixing ft_strlcat.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: vcarvalh <vcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 14:37:30 by vcarvalh          #+#    #+#             */
-/*   Updated: 2022/05/13 10:36:11 by vcarvalh         ###   ########.fr       */
+/*   Updated: 2022/05/17 08:25:02 by vcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,39 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	else
 		ret = srclen + size;
 	i = 0;
-	while (src[i] && dstlen < size - 1)
+	if (dstlen < size - 1 && size > 0)
 	{
-		dst[i] = src[i];
-		i++;
+		while (src[i] && dstlen + i < size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
 	}
 	dst[i] = '\0';
 	return (ret);
 }
+/*size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	dest_length;
+	size_t	src_length;
+
+	src_length = ft_strlen(src);
+	dest_length = ft_strlen(dst);
+	j = dest_length;
+	i = 0;
+	if (dest_length < size - 1 && size > 0)
+	{
+		while (src[i] && dest_length + i < size - 1)
+		{
+			dst[j] = src[i];
+			j++;
+			i++;
+		}
+		dst[j] = 0;
+	}
+	if (dest_length >= size)
+		dest_length = size;
+	return (dest_length + src_length);
+}*/
