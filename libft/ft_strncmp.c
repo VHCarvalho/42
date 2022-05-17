d57729c10d77cf50aa0288e2ca1dc6a07b51092e
vcarvalh <vh.crvlh@gmail.com>
2022-05-17 10:56:32 -0300

    Fixing ft_strncmp.c

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: vcarvalh <vcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 09:26:03 by vcarvalh          #+#    #+#             */
-/*   Updated: 2022/05/12 14:16:39 by vcarvalh         ###   ########.fr       */
+/*   Updated: 2022/05/17 10:53:35 by vcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	int				ret;
 
+	if (n <= 0)
+		return (0);
 	i = 0;
-	while (*s1 == *s2 && i < n && *s1 && *s2)
-	{
+	ret = 0;
+	while (s1[i] == s2[i] && i < n - 1 && s1[i] && s2[i])
 		i++;
-		s1++;
-		s2++;
-	}
-	return (*s1 - *s2);
+	ret = (unsigned char)s1[i] - (unsigned char)s2[i];
+	return (ret);
 }
