9a921f9440874babc6c87e2f4f989f439ff112a2
vcarvalh <vh.crvlh@gmail.com>
2022-05-17 10:54:50 -0300

    Fixing ft_strlcat.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: vcarvalh <vcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 14:37:30 by vcarvalh          #+#    #+#             */
-/*   Updated: 2022/05/17 08:25:02 by vcarvalh         ###   ########.fr       */
+/*   Updated: 2022/05/17 08:33:37 by vcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,28 +39,3 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	dst[i] = '\0';
 	return (ret);
 }
-/*size_t	ft_strlcat(char *dst, const char *src, size_t size)
-{
-	size_t	i;
-	size_t	j;
-	size_t	dest_length;
-	size_t	src_length;
-
-	src_length = ft_strlen(src);
-	dest_length = ft_strlen(dst);
-	j = dest_length;
-	i = 0;
-	if (dest_length < size - 1 && size > 0)
-	{
-		while (src[i] && dest_length + i < size - 1)
-		{
-			dst[j] = src[i];
-			j++;
-			i++;
-		}
-		dst[j] = 0;
-	}
-	if (dest_length >= size)
-		dest_length = size;
-	return (dest_length + src_length);
-}*/
