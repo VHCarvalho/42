37a225f472b27bee13d0b25858c2183de23872c7
vcarvalh <vh.crvlh@gmail.com>
2022-05-19 17:56:15 -0300

    end of day 05/19

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: vcarvalh <vcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 11:12:56 by vcarvalh          #+#    #+#             */
-/*   Updated: 2022/05/19 15:44:41 by vcarvalh         ###   ########.fr       */
+/*   Updated: 2022/05/19 17:55:46 by vcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,20 @@
 size_t	count_words(char *s, char c)
 {
 	size_t	i;
+	size_t	j;
+	char	last;
 
+	last = c;
 	i = 0;
-	while (*s)
+	j = 0;
+	while (s[i])
 	{
-		if (*s == c)
-		{
-			*s = '\0';
-			i++;
-		}
-		s++;
-	}
-	if (*(s - 1) != '\0')
+		if (s[i] != c && last == c)
+			j++;
+		last = s[i];
 		i++;
-	return (i);
+	}
+	return (j);
 }
 
 size_t	word_len(char const *s, char c)
@@ -47,35 +47,40 @@ char	**ft_split(char const *s, char c)
 	char	**ret;
 	size_t	count;
 	size_t	i;
-	//size_t	len;
-	//size_t	j;
+	size_t	len;
+	size_t	j;
 
 	count = count_words((char *)s, c);
-	ret = (char **)malloc(sizeof(char *) * count + 1);
+	ret = malloc(sizeof(char **) * count + 1);
 	i = 0;
-	//j = 0;
-	/*while (i < count)
+	j = 0;
+	while (i < count)
 	{
+		if (i == 0 && s[j] == c)
+		{
+			j++;
+			continue ;
+		}
 		if (i == 0)
 		{
-			len = ft_strchr(s, c) - s;
+			len = ft_strchr(s + j, c) - s;
 			ret[i] = ft_substr(s, j, len);
 			i++;
 		}
-		if (s[j] == c)
+		else if (s[j] == c)
 		{
-			len = ft_strchr(s + j + 1, c) - s;
-			ret[i] = ft_substr(s, j + 1, len);
+			len = ft_strchr(s + j - 1, c) - s;
+			ret[i] = ft_substr(s, j, len - 1);
 			i++;
 		}
 		j++;
-	}*/
-	while (i < count)
+	}
+	/*while (i < count)
 	{
 		ret[i] = ft_strdup(s);
 		s += ft_strlen(s) + 1;
 		i++;
-	}
+	}*/
 	ret[i] = malloc(sizeof(char *));
 	ret[i][0] = '\0';
 	return (ret);
@@ -83,14 +88,15 @@ char	**ft_split(char const *s, char c)
 
 int	main(void)
 {
-	char	s[] = "vzzzzaxxxxmos a xxxxla plxxxayxxxxa";
+	char	s[] = "aaavamos aaaaala playa";
 	char	c = 'a';
 	char	**ret;
 	int		i;
 
 	i = 0;
 	ret = ft_split(s, c);
-	while(ret[i])
+	printf("começando\n");
+	while(ret[i][0] != '\0')
 	{
 		printf("ret: %s\n", ret[i]);
 		i++;
