588186da6773cebf88219f7e366bc63ffea8252e
VH Carvalho <vh.crvlh@gmail.com>
2022-05-22 15:08:57 -0300

    continuing to implement the bonus functions

---
+++ libft/ft_lstlast_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vcarvalh <vcarvalh@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/22 14:39:10 by vcarvalh          #+#    #+#             */
+/*   Updated: 2022/05/22 14:42:15 by vcarvalh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*ptr;
+
+	if (!lst)
+		return (NULL);
+	ptr = lst;
+	while(ptr->next != NULL)
+		ptr = ptr->next;
+	return (ptr);
+}
