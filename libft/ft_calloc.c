70de64693adc19940c399460bf5362064f9e04cd
vcarvalh <vh.crvlh@gmail.com>
2022-05-25 08:15:27 -0300

    minor fix at ft_calloc.c

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: vcarvalh <vcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 12:18:45 by vcarvalh          #+#    #+#             */
-/*   Updated: 2022/05/22 14:34:24 by vcarvalh         ###   ########.fr       */
+/*   Updated: 2022/05/25 08:12:05 by vcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*ret;
 	size_t	total;
 
+	if (nmemb == 0 || size == 0)
+	{
+		nmemb = 1;
+		size = 1;
+	}
 	total = nmemb * size;
 	if (total / nmemb != size)
 		return (NULL);
