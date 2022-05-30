c64efc04daf2c91a949d9ced620c71cee2dc3e70
VH Carvalho <vh.crvlh@gmail.com>
2022-05-30 13:25:05 -0300

    just have to sort out the double free bug

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,69 +1,164 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vcarvalh <vcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/26 08:32:20 by vcarvalh          #+#    #+#             */
-/*   Updated: 2022/05/27 17:42:33 by vcarvalh         ###   ########.fr       */
+/*   Created: 2022/05/30 09:46:08 by vcarvalh          #+#    #+#             */
+/*   Updated: 2022/05/30 12:55:09 by vcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "get_next_line.h"
+#include <stdio.h>
+/*get_next_line:
+ *alocar buffer_size + 1
+ *checar o retorno de read
+ *se for maior que 0:
+ *	adicionar '\0' no final
+ *	concatenar com o stash
+ *	checar se o stash está com \n
+ *	se estiver:
+ *	separar, retornar tudo antes de \n e guardar o resto em stash
+ *se read for igual a 0:
+ *	dar free em buff e retornar stash
+ *	se read for menor que 0:
+ *	dar free em buff e stash e retornar null;
+ */
+
+static char	*store_buffer(char *buff, char *stash)
+{
+	size_t	i;
+	size_t	j;
+	char		*str;
+
+	if (!buff)
+		return (NULL) ;
+	i = 0;
+	if (!stash)
+	{
+		str = malloc(sizeof(char) * ft_strlen(buff) + 1);
+		while (buff[i])
+		{
+			str[i] = buff[i];
+			i++;
+		}
+		str[i] = '\0';
+		return (str);
+	}
+	str = malloc(sizeof(char) * (ft_strlen(buff) + ft_strlen(stash) + 1));
+	while (stash[i])
+	{
+		str[i] = stash[i];
+		i++;
+	}
+	j = 0;
+	while (buff[j])
+		str[i++] = buff[j++];
+	str[i] = '\0';
+	free(stash);
+	return (str);
+}
+
+static char	*get_line(char *stash, char *line)
+{
+	size_t	i;
+	size_t	len;
+
+	if (!stash)
+		return (NULL);
+	len = 0;
+	while (stash[len] != '\n')
+		len++;
+	line = malloc(sizeof(char) * len + 2);
+	i = 0;
+	while (stash[i] != '\n')
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	line[i] = '\n';
+	line[i + 1] = '\0';
+	return (line);
+}
+static char	*stash_leftovers(char *stash)
+{
+	size_t 	i;
+	size_t	j;
+	size_t 	new_len;
+	char		*str;
+
+	if (!stash)
+		return (NULL);
+	i = 0;
+	while (stash[i] != '\n')
+		i++;
+	i++;
+	j = i;
+	while (stash[i])
+	{
+		new_len++;
+		i++;
+	}
+	str = malloc(sizeof(char) * new_len + 1);
+	i = 0;
+	while (stash[j])
+		str[i++] = stash[j++];
+	str[i] = '\0';
+	free(stash);
+	return (str);
+}
 
 char	*get_next_line(int fd)
 {
-	static char	*stash;
-	char		*buff;
-	char		*line;
-	char		*tmp;
-	size_t		len;
-	int			check;
+	static char *stash;
+	char				*buff;
+	char				*line;
+	int					bytes;
 
 	if (fd < 0)
 		return (NULL);
 	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!buff)
 		return (NULL);
-	len = 0;
 	line = NULL;
-	check = read(fd, buff, BUFFER_SIZE);
-	printf("check: %d\n", check);
-	if (check == 0)
-		return (stash);
-	else if (check < 0)
-		return (NULL);
-	while (check > 0)
+	bytes = read(fd, buff, BUFFER_SIZE);
+	while (bytes > 0)
 	{
-		stash = ft_strjoin(stash, buff);
-		printf("%s\n", stash);
+		buff[bytes] = '\0';
+		stash = store_buffer(buff, stash);
 		if (ft_strchr(stash, '\n'))
 		{
-			while (stash[len] != '\n')
-				len++;
-			line = ft_substr(stash, 0, len);
-			tmp = ft_substr(stash, len, ft_strlen(stash));
-			free(stash);
-			stash = tmp;
-			break ;
+			line = get_line(stash, line);
+			stash = stash_leftovers(stash);
+			free(buff);
+			return (line);
 		}
-		check = read(fd, buff, BUFFER_SIZE);
-		printf("check: %d\n", check);
+		bytes = read(fd, buff, BUFFER_SIZE);
+	}
+	if (bytes == 0)
+	{
+		return (stash);
 	}
 	free(buff);
-	return (line);
+	return (NULL);
 }
 
 int	main(void)
 {
 	int		fd;
-	char 	*line;
+	char	*line;
 
 	fd = open("text4.txt", O_RDONLY);
 	line = get_next_line(fd);
-	printf("%s", line);
+	while (line)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
 	close(fd);
 	free(line);
+	return (0);
 }
