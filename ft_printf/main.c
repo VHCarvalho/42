368059e10cada7a201e548418d5a2f6b83daf4d1
vcarvalh <vh.crvlh@gmail.com>
2022-07-29 17:02:22 -0300

    end of day 07/29

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -4,12 +4,11 @@
 int main (void)
 {
 	int ret;
-	int *uns;
+	char uns[] = "playa";
 
-	&uns = 0;
-	ret = ft_printf("Vamos a la %p\n", &uns);
+	ret = ft_printf("Vamos a la %s\n", uns);
 	printf("%d\n", ret);
-	ret = printf("Vamos a la %p\n", &uns);
+	ret = printf("Vamos a la %s\n", uns);
 	printf("%d\n", ret);
 	return (0);
 }
