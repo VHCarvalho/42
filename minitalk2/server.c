f9e65a09947f78c87d50dac88a8995846ed6be7f
vcarvalh <vh.crvlh@gmail.com>
2022-09-13 09:53:23 -0300

    minitalk done

--- minitalk2/server.c
+++ minitalk2/server.c
@@ -6,12 +6,17 @@
 /*   By: vcarvalh <vcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 10:58:41 by vcarvalh          #+#    #+#             */
-/*   Updated: 2022/09/06 10:02:11 by vcarvalh         ###   ########.fr       */
+/*   Updated: 2022/09/13 09:30:24 by vcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void error()
+{
+	ft_putstr_fd("There was an unexpected error\n", 1);
+	exit(EXIT_FAILURE);
+}
 void	handler_sigusr(int signum, siginfo_t *info, void *context)
 {
 	static int						bit;
@@ -34,25 +39,25 @@ void	handler_sigusr(int signum, siginfo_t *info, void *context)
 		c = 0;
 	}
 	if (kill(pid, SIGUSR1) == -1)
-		write(1, "opa", 3);
+		error();
 }
 
 int	main(void)
 {
-	struct sigaction	sa_signal;
+	struct sigaction	sa;
 	sigset_t			block_mask;
 
 	sigemptyset(&block_mask);
 	sigaddset(&block_mask, SIGINT);
 	sigaddset(&block_mask, SIGQUIT);
-	sa_signal.sa_handler = 0;
-	sa_signal.sa_flags = SA_SIGINFO;
-	sa_signal.sa_mask = block_mask;
-	sa_signal.sa_sigaction = handler_sigusr;
-	sigaction(SIGUSR1, &sa_signal, NULL);
-	sigaction(SIGUSR2, &sa_signal, NULL);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_mask = block_mask;
+	sa.sa_sigaction = handler_sigusr;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	ft_putstr_fd("PID: ", 1);
 	ft_putnbr_fd(getpid(), 1);
-	ft_putstr_fd("\n", 1);
+	ft_putchar_fd('\n', 1);
 	while (1)
 		pause();
 }
