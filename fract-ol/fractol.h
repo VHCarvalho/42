2e9cc6dadabe757272bbd8e5d29867aae6e2e362
vcarvalh <vh.crvlh@gmail.com>
2023-01-20 17:21:50 -0300

    added rule to compile on mac and ESC handler

--- fract-ol/fractol.h
+++ fract-ol/fractol.h
@@ -6,7 +6,7 @@
 /*   By: vcarvalh <vcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 18:54:52 by vcarvalh          #+#    #+#             */
-/*   Updated: 2022/12/06 19:39:34 by vcarvalh         ###   ########.fr       */
+/*   Updated: 2023/01/20 14:52:24 by vcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,26 @@
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
-# include "mlx_linux/mlx.h"
 #include <math.h>
+# ifdef __linux__
+#  include "./mlx_linux/mlx.h"
+# else
+#  include "./mlx/mlx.h"
+# endif
 
+typedef struct	s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}	t_data;
 
+typedef struct s_vars
+{
+	void	*mlx;
+	void	*win;
+	t_data	img;
+} t_vars;
 #endif
--- fract-ol/main.c
+++ fract-ol/main.c
@@ -6,22 +6,13 @@
 /*   By: vcarvalh <vcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 19:18:03 by vcarvalh          #+#    #+#             */
-/*   Updated: 2023/01/07 14:49:25 by vcarvalh         ###   ########.fr       */
+/*   Updated: 2023/01/20 15:43:53 by vcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 #include <stdio.h>
 #include <unistd.h>
-
-typedef struct	s_data
-{
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}	t_data;
 
 void	my_mlx_pixel_put(t_data	*data, int x, int y, int color)
 {
@@ -39,17 +30,38 @@ void	put_line(t_data	*data, int x, int y, int length, int color)
 		length--;
 	}
 }
+int	mouse_hook(int keycode, int x, int y, void *vars)
+{
+	printf("%d\n", keycode);
+	(void)x;
+	(void)y;
+	(void)vars;
+	return (1);
+}
+
+int key_hook(int keycode, t_vars *vars)
+{
+	if (keycode == 53)
+	{
+		mlx_destroy_image(vars->mlx, vars->img.img);
+		mlx_destroy_window(vars->mlx, vars->win);
+	}
+	return (1);
+}
 int main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
+	//void	*mlx;
+	//void	*mlx_win;
+	//t_data	img;
+	t_vars	vars;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920/2, 1080/2, "test");
-	img.img = mlx_new_image(mlx, 1920/2, 1080/2);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	put_line(&img, 5, 5,100, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1920/2, 1080/2, "test");
+	vars.img.img = mlx_new_image(vars.mlx, 1920/2, 1080/2);
+	vars.img.addr = mlx_get_data_addr(vars.img.img, &vars.img.bits_per_pixel, &vars.img.line_length, &vars.img.endian);
+	put_line(&vars.img, 5, 5,100, 0x00FF0000);
+	mlx_put_image_to_window(vars.mlx, vars.win, vars.img.img, 0, 0);
+	mlx_mouse_hook(vars.win, mouse_hook, &vars);
+	mlx_key_hook(vars.win, key_hook, &vars);
+	mlx_loop(vars.mlx);
 }
